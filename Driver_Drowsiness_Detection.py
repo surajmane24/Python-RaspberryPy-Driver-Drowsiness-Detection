8811026c4eaf001d2ee39a88998fded65c425d84
Suraj S. Mane <83125278+surajmane24@users.noreply.github.com>
2021-08-14 23:25:44 +0530

    Create Driver_Drowsiness_Detection.py

---
+++ Driver_Drowsiness_Detection.py
@@ -0,0 +1,121 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import os
+
+def alarm(msg):
+    global alarm_status
+    global alarm_status2
+    global saying
+
+    while alarm_status:
+        print('call')
+        s = 'espeak "'+msg+'"'
+        os.system(s)
+        saying = False
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+                help="index of webcam on system")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 30
+alarm_status = False
+saying = False
+COUNTER = 0
+
+print("-> Loading the predictor and detector...")
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    #Faster but less accurate
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+print("-> Starting Video Stream")
+vs= VideoStream(usePiCamera=True).start()
+time.sleep(1.0)
+
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+                                    minNeighbors=5, minSize=(30, 30),
+                                    flags=cv2.CASCADE_SCALE_IMAGE)
+
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
+        
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        eye = final_ear(shape)
+        ear = eye[0]
+        leftEye = eye [1]
+        rightEye = eye[2]
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if alarm_status == False:
+                    alarm_status = True
+                    t = Thread(target=alarm, args=('wake up sir',))
+                    t.deamon = True
+                    t.start()
+
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        else:
+            COUNTER = 0
+            alarm_status = False
+
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
